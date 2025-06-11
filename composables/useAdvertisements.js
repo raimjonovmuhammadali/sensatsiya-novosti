cc1313238d137e30da40f1b3a41fac881df8bb4e
raimjonovmuhammadali <raimjonovblog@gmail.com>
2025-06-12 03:23:29 +0500

    updateuseapi

--- composables/useAdvertisements.js
+++ composables/useAdvertisements.js
@@ -1,59 +1,59 @@
-import { ref } from 'vue';
-import { useApi } from './useApi';
+// import { ref } from 'vue';
+// import { useApi } from './useApi';
 
-export function useAdvertisements() {
-  const { fetch } = useApi();
-  const error = ref(null);
+// export function useAdvertisements() {
+//   const { fetch } = useApi();
+//   const error = ref(null);
 
-  const getAllAdvertisements = async () => {
-    const { data, error: fetchError } = await fetch('advertisements/');
-    if (fetchError) {
-      error.value = 'Reklamalarni yuklashda xatolik yuz berdi';
-      return [];
-    }
-    return data || [];
-  };
+//   const getAllAdvertisements = async () => {
+//     const { data, error: fetchError } = await fetch('advertisements/');
+//     if (fetchError) {
+//       error.value = 'Reklamalarni yuklashda xatolik yuz berdi';
+//       return [];
+//     }
+//     return data || [];
+//   };
 
-  const addAdvertisement = async (adData) => {
-    const { error: postError } = await fetch('advertisements/', {
-      method: 'POST',
-      body: JSON.stringify(adData),
-    });
-    if (postError) {
-      error.value = 'Reklama qo‘shishda xatolik yuz berdi';
-      return false;
-    }
-    return true;
-  };
+//   const addAdvertisement = async (adData) => {
+//     const { error: postError } = await fetch('advertisements/', {
+//       method: 'POST',
+//       body: JSON.stringify(adData),
+//     });
+//     if (postError) {
+//       error.value = 'Reklama qo‘shishda xatolik yuz berdi';
+//       return false;
+//     }
+//     return true;
+//   };
 
-  const updateAdvertisement = async (id, adData) => {
-    const { error: putError } = await fetch(`advertisements/${id}/`, {
-      method: 'PUT',
-      body: JSON.stringify(adData),
-    });
-    if (putError) {
-      error.value = 'Reklamani tahrirlashda xatolik yuz berdi';
-      return false;
-    }
-    return true;
-  };
+//   const updateAdvertisement = async (id, adData) => {
+//     const { error: putError } = await fetch(`advertisements/${id}/`, {
+//       method: 'PUT',
+//       body: JSON.stringify(adData),
+//     });
+//     if (putError) {
+//       error.value = 'Reklamani tahrirlashda xatolik yuz berdi';
+//       return false;
+//     }
+//     return true;
+//   };
 
-  const deleteAdvertisement = async (id) => {
-    const { error: deleteError } = await fetch(`advertisements/${id}/`, {
-      method: 'DELETE',
-    });
-    if (deleteError) {
-      error.value = 'Reklamani o‘chirishda xatolik yuz berdi';
-      return false;
-    }
-    return true;
-  };
+//   const deleteAdvertisement = async (id) => {
+//     const { error: deleteError } = await fetch(`advertisements/${id}/`, {
+//       method: 'DELETE',
+//     });
+//     if (deleteError) {
+//       error.value = 'Reklamani o‘chirishda xatolik yuz berdi';
+//       return false;
+//     }
+//     return true;
+//   };
 
-  return {
-    error,
-    getAllAdvertisements,
-    addAdvertisement,
-    updateAdvertisement,
-    deleteAdvertisement,
-  };
-}
+//   return {
+//     error,
+//     getAllAdvertisements,
+//     addAdvertisement,
+//     updateAdvertisement,
+//     deleteAdvertisement,
+//   };
+// }
--- composables/useMedias.js
+++ composables/useMedias.js
@@ -1,59 +1,59 @@
-import { ref } from 'vue';
-import { useApi } from './useApi';
+// import { ref } from 'vue';
+// import { useApi } from './useApi';
 
-export function useMedias() {
-  const { fetch } = useApi();
-  const error = ref(null);
+// export function useMedias() {
+//   const { fetch } = useApi();
+//   const error = ref(null);
 
-  const getAllMedias = async () => {
-    const { data, error: fetchError } = await fetch('medias/');
-    if (fetchError) {
-      error.value = 'Ijtimoiy tarmoqlarni yuklashda xatolik yuz berdi';
-      return [];
-    }
-    return data || [];
-  };
+//   const getAllMedias = async () => {
+//     const { data, error: fetchError } = await fetch('medias/');
+//     if (fetchError) {
+//       error.value = 'Ijtimoiy tarmoqlarni yuklashda xatolik yuz berdi';
+//       return [];
+//     }
+//     return data || [];
+//   };
 
-  const addMedia = async (mediaData) => {
-    const { error: postError } = await fetch('medias/', {
-      method: 'POST',
-      body: JSON.stringify(mediaData),
-    });
-    if (postError) {
-      error.value = 'Ijtimoiy tarmoq qo‘shishda xatolik yuz berdi';
-      return false;
-    }
-    return true;
-  };
+//   const addMedia = async (mediaData) => {
+//     const { error: postError } = await fetch('medias/', {
+//       method: 'POST',
+//       body: JSON.stringify(mediaData),
+//     });
+//     if (postError) {
+//       error.value = 'Ijtimoiy tarmoq qo‘shishda xatolik yuz berdi';
+//       return false;
+//     }
+//     return true;
+//   };
 
-  const updateMedia = async (id, mediaData) => {
-    const { error: putError } = await fetch(`medias/${id}/`, {
-      method: 'PUT',
-      body: JSON.stringify(mediaData),
-    });
-    if (putError) {
-      error.value = 'Ijtimoiy tarmoqni tahrirlashda xatolik yuz berdi';
-      return false;
-    }
-    return true;
-  };
+//   const updateMedia = async (id, mediaData) => {
+//     const { error: putError } = await fetch(`medias/${id}/`, {
+//       method: 'PUT',
+//       body: JSON.stringify(mediaData),
+//     });
+//     if (putError) {
+//       error.value = 'Ijtimoiy tarmoqni tahrirlashda xatolik yuz berdi';
+//       return false;
+//     }
+//     return true;
+//   };
 
-  const deleteMedia = async (id) => {
-    const { error: deleteError } = await fetch(`medias/${id}/`, {
-      method: 'DELETE',
-    });
-    if (deleteError) {
-      error.value = 'Ijtimoiy tarmoqni o‘chirishda xatolik yuz berdi';
-      return false;
-    }
-    return true;
-  };
+//   const deleteMedia = async (id) => {
+//     const { error: deleteError } = await fetch(`medias/${id}/`, {
+//       method: 'DELETE',
+//     });
+//     if (deleteError) {
+//       error.value = 'Ijtimoiy tarmoqni o‘chirishda xatolik yuz berdi';
+//       return false;
+//     }
+//     return true;
+//   };
 
-  return {
-    error,
-    getAllMedias,
-    addMedia,
-    updateMedia,
-    deleteMedia,
-  };
-}
+//   return {
+//     error,
+//     getAllMedias,
+//     addMedia,
+//     updateMedia,
+//     deleteMedia,
+//   };
+// }
