b203524b9043a84f628423a14a25569efd94a2d6
raimjonovmuhammadali <raimjonovblog@gmail.com>
2025-06-19 16:26:11 +0500

    finishedupdates

--- composables/useApi.js
+++ composables/useApi.js
@@ -1,1 +1,1 @@
-export const BASE_URL = 'http://45.144.221.41/';
+export const BASE_URL = 'https://api.sensatsiya-novosti.uz/';
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,7 @@
 import tailwindcss from "@tailwindcss/vite";
 export default defineNuxtConfig({
   compatibilityDate: "2025-05-15",
+  ssr: false,
   css: ["~/assets/css/main.css"],
   vite: {
     plugins: [tailwindcss()],
